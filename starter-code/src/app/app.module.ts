ec9d0e6a5345ec71e4289cbebb42a8d7e5ad3d08
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-04 23:03:37 +0200

    add routes and router outlet

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,23 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
-import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
+import {RouterModule, Routes} from '@angular/router';
 import { MyHomeComponent } from './my-home/my-home.component';
 import { MyMovieComponent } from './my-movie/my-movie.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'movie/:id', component: MyMovieComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyHomeComponentComponent,
-    MyMovieComponentComponent,
     MyHomeComponent,
     MyMovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
