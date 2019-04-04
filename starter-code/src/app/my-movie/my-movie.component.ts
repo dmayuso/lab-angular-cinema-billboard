ffc3f31fd60be7fd3ad6e3f8f3eba63004a910ea
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-04 23:43:48 +0200

    implement movie component

--- starter-code/src/app/my-movie/my-movie.component.ts
+++ starter-code/src/app/my-movie/my-movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MoviesService} from '../services/movies.service';
+import {CinemaRoom} from '../domain/cinema-room';
 
 @Component({
   selector: 'app-my-movie',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyMovieComponent implements OnInit {
 
-  constructor() { }
+  movieId: number;
+  movie: CinemaRoom;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private moviesService: MoviesService) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params) => this.movieId = Number(params['id']));
+    this.movie = this.moviesService.getMovie(this.movieId);
   }
 
+  goHome() {
+    this.router.navigate(['home']);
+  }
 }
